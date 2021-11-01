1dcaf0e2d0ad59cb697d06700d95b545efc87020
SonOfGrabarz <m.szul00@gmail.com>
2021-11-01 10:04:42 +0100

    lista2-1

--- lista2/main.cpp
+++ lista2/main.cpp
@@ -1,41 +1,38 @@
 #include <iostream>
 
-// zadanie 1
-template <typename T, typename U>
-auto add1(T a, U b){
-    return a + b;
-}
-
+// zadanie 5
 template <typename T, typename U>
 auto add1(T *a, U *b){
     return *a + *b;
 }
 
+template <typename T, typename U>
+auto add2(const T x, const U y){
+    std::string a = x;
+    std::string b = y;
+    return a + b;
+}
 
-// template <typename T, typename U, typename W>
-// auto add2(T x, U y, W z){
-//     return z(x, y);
-// }
+// zadnie 6
 
 int main(){
 
+    // zadanie 5
+    std::cout << "ZADANIE5" << '\n';
     int num1, num2;
-    int *ptr1, *ptr2;
-    const char* str1;
-    const char* str2;
+    int *num_ptr1, *num_ptr2;
 
-    num1 = 4;
-    num2 = 5;
-    char first = 'C';
-    char second = 'J';
+    num1 = 21;
+    num2 = 11;
 
-    ptr1 = &num1;
-    ptr2 = &num2;
-    str1 = &first;
-    str2 = &second;
+    const char* first = "M";
+    const char* second = "S";
 
-    std::cout << add1(ptr1, ptr2) << '\n';
-    std::cout << add1(str1, str2) << '\n';
+    num_ptr1 = &num1;
+    num_ptr2 = &num2;
+
+    std::cout << add1(num_ptr1, num_ptr2) << '\n';
+    std::cout << add2(first, second) << "\n\n";
 
     return 0;
 }
