901bad5266f5048b5c8c6aa1ba35c636595f8809
SonOfGrabarz <m.szul00@gmail.com>
2021-11-04 12:02:34 +0100

    lista2-final

--- lista2/main.cpp
+++ lista2/main.cpp
@@ -3,12 +3,14 @@
 
 // zadanie 5
 template <typename T, typename U>
-auto add1(T *a, U *b){
+auto add1(T *a, U *b) {
+    // przekazanie przez wskazniki
     return *a + *b;
 }
 
 template <typename T, typename U>
-auto add2(const T x, const U y){
+auto add2(const T x, const U y) {
+    // laczenie tekstow przekazanych jako const char*
     std::string a = x;
     std::string b = y;
     return a + b;
@@ -17,14 +19,29 @@ auto add2(const T x, const U y){
 // zadanie 6
 template <typename T, typename U>
 constexpr auto hiperszescian(T a, U b) -> decltype (pow(a, b)){
+    // przypadek kiedy podamy ujemna dlugosc boku lub ilosc wymiaru
     if (a < 0 || b < 0){
         return -1;
     }
 
+    // obliczenie objetosci hiperszescianu
     else{
         return pow(a, b);
     }
 }
+
+// zadanie 7 (z wykladu)
+template <typename T, typename ... Args>
+T add(T first, Args ... args) {
+    if constexpr(sizeof...(args) == 0) {
+        return first;
+    } 
+
+    else {
+        return first + add(args...);
+    }
+}
+
 
 int main(){
 
@@ -48,6 +65,10 @@ int main(){
     // zadanie 6
     std::cout << "ZADANIE6" << '\n';
     std::cout << "HIPERSZESCIAN(5 BOKOW, 6 WYMIAROW): " << hiperszescian(6.5f, 7) << "\n\n";
+
+    // zadanie 7
+    std::cout << "ZADANIE7" << '\n';
+    std::cout << add(1, 1.0, 1.0f) << '\n';
 
 
     return 0;
